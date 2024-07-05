a0eafd90d2eca1f1a1297a6855fb4ce0a83da072
DongHyeonWon <dhe7700@naver.com>
2024-07-05 09:40:39 +0900

    feat: 캐릭터 추가

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,19 +1,16 @@
-import { OrbitControls } from "@react-three/drei";
-
+import { Environment, OrbitControls } from "@react-three/drei";
+import Character from "./components/characters/Character";
 export default function Experience() {
   return (
     <>
       <OrbitControls makeDefault enablePan={false} />
-      <ambientLight intensity={0.2} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
-      <mesh position={[0, 1, 1]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="orange" />
+      {/* <ambientLight intensity={0.2} /> */}
+      <directionalLight position={[10, 10, 5]} intensity={0.1} />
+      <Environment preset="city" />
+      <mesh position={[0, -2, 0]} rotation-y={-0.05}>
+        <boxGeometry args={[100, 1, 100]} /> <meshBasicMaterial color="gray" />
       </mesh>
-      <mesh rotation-y={-0.05}>
-        <boxGeometry args={[100, 1, 100]} />{" "}
-        <meshBasicMaterial color="orange" />
-      </mesh>
+      <Character />
     </>
   );
 }
